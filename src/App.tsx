d8d911cdd31d54dee99824804c605144328425eb
Duulat Akmatbekov <duulat.akmatbekov@gmail.com>
2022-11-13 20:41:48 +0600

    Added components: Modal and Backdrop.
    Added all functions to cancel and showModal state to control Modal in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Modal from "./components/Modal/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const cancel = () => setShowModal(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-center my-3">
+      <Modal show={showModal} title={"Some kinda modal title"} onClose={cancel}>
+        <div className="modal-body">
+          This is modal content
+        </div>
+        <div className="modal-footer">
+          <button className="btn btn-danger" onClick={cancel}>
+            Cancel
+          </button>
+        </div>
+      </Modal>
+      <button className="btn btn-primary" onClick={() => setShowModal(true)}>Show</button>
     </div>
   );
 }
